637bb07828eb616c951a395edc88144d45354ea5
Futar <Futarimiti@163.com>
2023-12-03 19:47:17 +0000

    day2: remove needy main2

--- 2-cube-conudrum/main.py
+++ 2-cube-conudrum/main.py
@@ -1,9 +1,7 @@
-# pyright: strict
-
 from enum import Enum
-from dataclasses import dataclass, replace
-from typing import List, Tuple
 from itertools import dropwhile, takewhile
+from typing import List, Tuple
+from dataclasses import dataclass, replace
 from functools import reduce
 
 class Colour(Enum):
@@ -13,8 +11,8 @@ class Colour(Enum):
 
 @dataclass
 class Take:
-    red: int
     green: int
+    red: int
     blue: int
 
 @dataclass
@@ -22,55 +20,10 @@ class Game:
     id: int
     takes: List[Take]
 
-# parse one line of game
-def parse_line(line: str) -> Game:
-    id = parse_id(line)
-    without_game_label = ''.join(list(dropwhile(lambda c: c != ':', line))[2:])  # drop ': '
-    takes_str = without_game_label.split('; ')
-    takes = list(map(parse_take, takes_str))
-    return Game(id=id, takes=takes)
-
-def parse_id(line: str) -> int:
-    game_label = takewhile(lambda c: c != ':', line)
-    game_id_char_list = list(game_label)[5:]  # drop 'Game '
-    game_id_str = ''.join(game_id_char_list)
-    return int(game_id_str)
-
-# parse one take of cubes
-# '3 blue, 4 red' -> Take(blue=3, red=4, green=0)
-def parse_take(s: str) -> Take:
-    cubes_str = s.split(', ')
-    cubes = map(parse_cubes, cubes_str)
-
-    def temp(take: Take, cubes: Tuple[int, Colour]) -> Take:
-        (amount, colour) = cubes
-        match colour:
-            case Colour.RED:
-                return replace(take, red=take.red + amount)
-            case Colour.GREEN:
-                return replace(take, green=take.green + amount)
-            case Colour.BLUE:
-                return replace(take, blue=take.blue + amount)
-
-    return reduce(temp, cubes, Take(red=0, green=0, blue=0))
-
-# parse something like '3 blue', '4 red'
-# '20 red' -> (20, RED)
-def parse_cubes(s: str) -> Tuple[int, Colour]:
-    [amount_str, colour_str] = s.split(' ')
-    amount = int(amount_str)
-    colour = read_colour(colour_str)
-    return (amount, colour)
-
-bag: Take = Take(red=12, green=13, blue=14)
-
-def possible_take(bag: Take, take: Take) -> bool:
-    return all([bag.red >= take.red, bag.green >= take.green, bag.blue >= take.blue])
-
-def possible_game(bag: Take, game: Game) -> bool:
-    return all(map(lambda take: possible_take(bag, take), game.takes))
-
-def read_colour(s: str) -> Colour:
+# 'red' -> RED
+# 'green' -> GREEN
+# 'blue' -> BLUE
+def parse_colour(s: str) -> Colour:
     match s:
         case 'red':
             return Colour.RED
@@ -79,18 +32,83 @@ def read_colour(s: str) -> Colour:
         case 'blue':
             return Colour.BLUE
         case _:
-            raise Exception(f'Invalid colour: "{s}"')
+            raise ValueError(f'Invalid colour: "{s}"')
+
+# '3 blue' -> (BLUE, 3)
+# '4 red' -> (RED, 4)
+def parse_cubes(s: str) -> Tuple[Colour, int]:
+    [amount_str, colour_str] = s.split(' ')
+    amount = int(amount_str)
+    colour = parse_colour(colour_str)
+    return colour, amount
+
+# '3 blue, 4 red' -> Take(blue=3, red=4, green=0)
+# '1 red, 2 green, 6 blue' -> Take(red=1, green=2, blue=6)
+def parse_take(s: str) -> Take:
+    cubes_str = s.split(', ')
+    cubes = map(parse_cubes, cubes_str)
+    
+    def f(acc: Take, x: Tuple[Colour, int]) -> Take:
+        (colour, amount) = x
+        match colour:
+            case Colour.RED:
+                return replace(acc, red=acc.red + amount)
+            case Colour.GREEN:
+                return replace(acc, green=acc.green + amount)
+            case Colour.BLUE:
+                return replace(acc, blue=acc.blue + amount)
+
+    return reduce(f, cubes, Take(green=0, red=0, blue=0))
+
+# '3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green' -> [Take(blue=3, red=4, green=0), Take(red=1, green=2, blue=6), Take(green=2, red=0, blue=0)]
+def parse_takes(s: str) -> List[Take]:
+    takes_str = s.split('; ')
+    takes = map(parse_take, takes_str)
+    return list(takes)
+
+# 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green'
+# -> Game(id=1, takes=[Take(blue=3, red=4, green=0), Take(red=1, green=2, blue=6), Take(green=2, red=0, blue=0)])
+def parse_line(line: str) -> Game:
+    line_without_game = line[5:]
+    id_str = ''.join(takewhile(lambda c: c.isnumeric(), line_without_game))
+    id = int(id_str)
+
+    until_colon = ''.join(dropwhile(lambda c: c != ':', line))
+    takes_str = until_colon[2:]
+    takes = parse_takes(takes_str)
+
+    return Game(id=id, takes=takes)
+
+def is_possible_take(take: Take) -> bool:
+    return take.red <= 12 and take.green <= 13 and take.blue <= 14
+
+def is_possible_game(game: Game) -> bool:
+    return all(map(is_possible_take, game.takes))
+
+
+# part 2
+
+def fewest(takes: List[Take]) -> Take:
+    most_red = max(map(lambda t: t.red, takes))
+    most_green = max(map(lambda t: t.green, takes))
+    most_blue = max(map(lambda t: t.blue, takes))
+    return Take(red=most_red, green=most_green, blue=most_blue)
+
+def power(take: Take) -> int:
+    return take.red * take.green * take.blue
 
 def main():
-    with open('input.txt', 'r') as f:
-        lines = [line[:-1] for line in f.readlines()]
-
+    handle = open('input.txt', 'r')
+    text = handle.read()
+    lines = text.splitlines()
     games = map(parse_line, lines)
-    possible_games = filter(lambda game: possible_game(bag, game), games)
-    possible_game_ids = map(lambda game: game.id, possible_games)
-    id_sum = sum(possible_game_ids)
-
-    print(id_sum)
+    # possible_games = filter(is_possible_game, games)
+    # total = sum(map(lambda g: g.id, possible_games))
+    # print(total)
+    takes = map(lambda g: g.takes, games)
+    fewests = map(fewest, takes)
+    powers = map(power, fewests)
+    print(sum(powers))
 
 if __name__ == '__main__':
     main()
