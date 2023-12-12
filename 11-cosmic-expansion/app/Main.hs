a46afdbbdcf4811f69e3af42247d79599609738b
Futar <Futarimiti@163.com>
2023-12-12 02:13:04 +0000

    cleanup

--- 11-cosmic-expansion/app/Main.hs
+++ 11-cosmic-expansion/app/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
 
 module Main (main) where
 
@@ -24,37 +25,30 @@ parseLine :: String -> [Int]
 parseLine = elemIndices '#'
 
 galaxyPairs :: Image -> [(Coords, Coords)]
-galaxyPairs Image{galaxies} = [(x, y) | (x:rest) <- tails galaxies, y <- rest]
+galaxyPairs Image{..} = [(x, y) | (x:rest) <- tails galaxies, y <- rest]
 
 expand :: Image -> Image
 expand i@Image{..} = expandCols emptyCols $ expandRows emptyRows i
-  where -- [*, *, <empty>, *, *] -> [*, *, <empty>, <empty>, <empty>, *, *]
-        expandCols :: [Int] -> Image -> Image
-        expandCols xs i = foldr expandCol i xs
+  where expandCols :: [Int] -> Image -> Image
+        expandCols xs n = foldr expandCol n xs
 
         expandCol :: Int -> Image -> Image
         -- part 1
-        -- expandCol n i@Image{..} = Image{galaxies=fmap (\(row, col) -> if col > n then (row, col + 1) else (row, col)) galaxies, width=width+1, height=height}
+        -- expandCol n Image{..} = Image{galaxies=fmap (\(row, col) -> if col > n then (row, col + 1) else (row, col)) galaxies, width=width+1, height=height}
         -- part 2
-        expandCol n i@Image{..} = Image{galaxies=fmap (\(row, col) -> if col > n then (row, col + million) else (row, col)) galaxies, width=width+million, height=height}
+        expandCol n Image{..} = Image{galaxies=fmap (\(row, col) -> if col > n then (row, col + million) else (row, col)) galaxies, width=width+million, height=height}
 
         expandRows :: [Int] -> Image -> Image
-        expandRows xs i = foldr expandRow i xs
+        expandRows xs n = foldr expandRow n xs
 
         expandRow :: Int -> Image -> Image
         -- part 1
-        -- expandRow n i@Image{..} = Image{galaxies=fmap (\(row, col) -> if row > n then (row + 1, col) else (row, col)) galaxies, width=width, height=height+1}
+        -- expandRow n Image{..} = Image{galaxies=fmap (\(row, col) -> if row > n then (row + 1, col) else (row, col)) galaxies, width=width, height=height+1}
         -- part 2
-        expandRow n i@Image{..} = Image{galaxies=fmap (\(row, col) -> if row > n then (row + million, col) else (row, col)) galaxies, width=width, height=height+million}
+        expandRow n Image{..} = Image{galaxies=fmap (\(row, col) -> if row > n then (row + million, col) else (row, col)) galaxies, width=width, height=height+million}
 
         million = 1000000 - 1  -- ;p
-
-        -- [8,14,20,47,48,51,92,93,120]
-        emptyRows :: [Int]
         emptyRows = [0 .. width - 1] \\ map fst galaxies
-
-        -- [30,33,50,57,66,74,75,77,80,93,121,135]
-        emptyCols :: [Int]
         emptyCols = [0 .. height - 1] \\ map snd galaxies
 
 
