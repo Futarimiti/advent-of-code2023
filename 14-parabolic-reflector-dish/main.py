097dd9ca74bd9c799c13629c81eded6055143d32
Futar <Futarimiti@163.com>
2023-12-16 16:16:22 +0000

    day 14: style

--- 14-parabolic-reflector-dish/main.py
+++ 14-parabolic-reflector-dish/main.py
@@ -4,38 +4,30 @@ from dataclasses import dataclass, replace
 from itertools import count, repeat
 from functools import reduce
 from copy import copy
-
 from collections.abc import Callable
 import sys
 
 
 def tailrec(g: Callable) -> Callable:
-	class TailRecurseException(BaseException):
-		def __init__(self, args, kwargs):
-			self.args = args
-			self.kwargs = kwargs
-	"""
-	This function decorates a function with tail call
-	optimization. It does this by throwing an exception
-	if it is its own grandparent, and catching such
-	exceptions to fake the tail call optimization.
 
-	This function fails if the decorated
-	function recurses in a non-tail context.
-	"""
-	def func(*args, **kwargs):
-		f = sys._getframe()
-		if f.f_back and f.f_back.f_back \
-				and f.f_back.f_back.f_code == f.f_code:
-			raise TailRecurseException(args, kwargs)
-		else:
-			while 1:
-				try: return g(*args, **kwargs)
-				except TailRecurseException as e:
-					args = e.args
-					kwargs = e.kwargs
-	func.__doc__ = g.__doc__
-	return func
+    class TailRecurseException(BaseException):
+        def __init__(self, args, kwargs):
+            self.args = args
+            self.kwargs = kwargs
+
+    def func(*args, **kwargs):
+        f = sys._getframe()
+        if f.f_back and f.f_back.f_back and f.f_back.f_back.f_code == f.f_code:
+            raise TailRecurseException(args, kwargs)
+        else:
+            while True:
+                try: return g(*args, **kwargs)
+                except TailRecurseException as e:
+                    args = e.args
+                    kwargs = e.kwargs
+
+    func.__doc__ = g.__doc__
+    return func
 
 
 class Direction(Enum):
